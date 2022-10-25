ba6d35218470b6b9cc9483d4eff9127e8e9ec0bb
JReuman1 <99495156+JReuman1@users.noreply.github.com>
2022-10-25 18:12:28 +0200

    Create Pinia Store

---
+++ project/src/components/NavBar.vue
@@ -0,0 +1,14 @@
+<template>
+</template>
+
+<script>
+export default {
+  setup() {
+    return {
+    };
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ project/src/stores/contract.js
@@ -0,0 +1,12 @@
+import { defineStore } from "pinia";
+
+export default defineStore("LogIn", {
+  state() {
+    return {
+      //Acá van las variables.
+    };
+  },
+  actions: {
+    //Acá van las funciones.
+  },
+});
