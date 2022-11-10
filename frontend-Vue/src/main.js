186efc0ca6b457f9727f207e1d12f73daa8c55b3
JReuman1 <99495156+JReuman1@users.noreply.github.com>
2022-11-10 16:07:56 +0100

    Added fontawesome icons

--- frontend-Vue/src/main.js
+++ frontend-Vue/src/main.js
@@ -4,6 +4,9 @@ import App from './App.vue'
 import "bootstrap/dist/css/bootstrap.css"
 import "bootstrap/dist/js/bootstrap.js"
 
+import "@fortawesome/fontawesome-free/css/all.css"
+import "@fortawesome/fontawesome-free/js/all.js"
+
 import { createPinia } from "pinia"
 
 //import router from "./router/index"
