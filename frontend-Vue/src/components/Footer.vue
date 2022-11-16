899f38f0a547af752b047d9411533df9305fddb2
JReuman1 <99495156+JReuman1@users.noreply.github.com>
2022-11-16 15:27:22 +0100

    Change app name in footer component

--- frontend-Vue/src/components/Footer.vue
+++ frontend-Vue/src/components/Footer.vue
@@ -23,7 +23,7 @@
     <!-- Copyright -->
     <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2)">
       © 2022 Copyright:
-      <a class="text-white" href="">AGRO Lend</a>
+      <a class="text-white" href="">Crypto Lend</a>
     </div>
     <!-- Copyright -->
   </footer>
