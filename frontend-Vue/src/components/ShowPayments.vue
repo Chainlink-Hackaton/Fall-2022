b5fb68a7faeef94c6e1fdf7086b05b5d40765e82
JReuman1 <99495156+JReuman1@users.noreply.github.com>
2022-11-18 15:20:38 +0100

    Added changes

--- frontend-Vue/src/components/ShowPayments.vue
+++ frontend-Vue/src/components/ShowPayments.vue
@@ -1,29 +1,36 @@
 <template>
-    <!--<div v-for="(id, index) in RegistryStore.txhash" :key="index">-->
-        <div>
-        <div class="form animated flipInX">
-            <h2>Check Payments</h2>
-            <br />
-            <form>
-                <h6>Debt hash:</h6>
-                <input v-model="RegistryStore.txhash" placeholder="Transaction Hash" list="txhash" name="txhash"
-                    type="text" />
-                <h6>Payment hash:</h6>
-                <!--<input v-for="(payment, index2) in RegistryStore.txhash[id]" disabled
-                    v-model="RegistryStore.txhash[id].payment[index2]" placeholder="Lender Address" type="text" />-->
-
-                    <inputdisabled
-                    v-model="RegistryStore.txhash" placeholder="Lender Address" type="text" />
-
-
-                <button class="registerPayment"
-                    @click.prevent="RegistryStore.registerPayment(RegistryStore.debts[index].Id,)">Accept
-                    Payment</button>
-
-            </form>
-        </div>
+    <div
+    v-for="(debt, index) in RegistryStore.tx"
+    :key="index">
+      <div class="form animated flipInX">
+        <h2>Accept a Debt</h2>
+        <h6 v-if="RegistryStore.debts[index].status === '0'">Debt status: <h6 class="text-warning">Pending</h6></h6>
+        <h6 v-else-if="RegistryStore.debts[index].status === '1'">Debt status:  <h6 class="text-success">Approved</h6></h6>
+        <h6 v-else-if="RegistryStore.debts[index].status === '2'">Debt status:  <h6 class="text-danger">Rejected</h6></h6>
+        <h6 v-else-if="RegistryStore.debts[index].status === '3'">Debt status:  <h6 class="text-approved">Paid</h6></h6>
+        <h6 v-else>Debt status: <h6 class="text-secondary">Default</h6></h6>
+    
+        <br />
+        <form>
+          <input disabled v-model="RegistryStore.debts[index].Owner" placeholder="Lender Address" type="text" />
+    
+          <input disabled v-model="RegistryStore.debts[index].Currency" placeholder="Currency" list="currencys" name="currency" id="list" />
+    
+          <input disabled v-model="RegistryStore.debts[index].Amount" placeholder="Amount" type="text" />
+    
+          <input disabled v-model="RegistryStore.debts[index].Deadline" placeholder="Time to Pay" list="timeToPay" name="timeToPay" id="list" />
+    
+    
+          <input disabled v-model="RegistryStore.debts[index].Split" placeholder="Number of Payments" list="numberOfPayments" name="numberOfPayments" id="list" />
+    
+        
+          <button class="acceptDebt" @click.prevent="RegistryStore.acceptDebt(RegistryStore.debts[index].Id)">Accept Debt</button>
+          <button class="rejectDebt" @click.prevent="RegistryStore.rejectDebt(RegistryStore.debts[index].Id)">Reject Debt</button>
+    
+        </form>
+      </div>
     </div>
-</template>
+    </template>
     <!--
         address lender,
             address currency,
@@ -32,106 +39,111 @@
             uint numberOfPayments
     -->
     
-<script>
-import { ref } from "vue"
-import { mapStores } from "pinia";
-import useRegistryStore from "../stores/registry.js"
-
-export default {
-    name: "ShowPayments",
-    setup() {
+    <script>
+    import { ref } from "vue"
+    import {mapStores} from "pinia";
+    import useRegistryStore from "../stores/registry.js"
+    
+    export default {
+      name: "ShowPayments",
+      setup() {
         return {
         };
-    },
-
-    methods: {
-
-    },
-
-    computed: {
-        ...mapStores(useRegistryStore)
-    },
-
-    mounted() {
-        this.RegistryStore.connect();
-    }
-};
-</script>
+      },
     
-<style>
-.form {
-    display: flex;
-    flex-wrap: nowrap;
-
-    justify-content: center;
-    flex-direction: column;
-    background: #fff;
-    margin-top: 5%;
-    margin-left: 20%;
-    margin-right: 20%;
-    margin-bottom: 5%;
-    padding: 30px;
-    box-shadow: 0 0 3px rgba(0, 0, 0, 0.3);
-}
-
-h6 {
-    color: #44c4e7;
-}
-
-.form h2 {
-    margin: 0 0 20px;
-    line-height: 1;
-    color: #44c4e7;
-    font-size: 18px;
-    font-weight: 400;
-}
-
-.form input {
-    outline: none;
-    display: block;
-    width: 100%;
-    margin: 0 0 20px;
-    padding: 10px 15px;
-    border: 1px solid #ccc;
-    color: #ccc;
-    font-family: "Roboto";
-    -webkit-box-sizing: border-box;
-    -moz-box-sizing: border-box;
-    box-sizing: border-box;
-    font-size: 14px;
-    font-weight: 400;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    -webkit-transition: 0.2s linear;
-    -moz-transition: 0.2s linear;
-    -ms-transition: 0.2s linear;
-    -o-transition: 0.2s linear;
-    transition: 0.2s linear;
-}
-
-.form input:focus {
-    color: #333;
-    border: 1px solid #44c4e7;
-}
-
-.form button {
-    cursor: pointer;
-    background: #44c4e7;
-    width: 100%;
-    padding: 10px 15px;
-    border: 0;
-    color: #fff;
-    font-family: "Roboto";
-    font-size: 14px;
-    font-weight: 400;
-}
-
-.form button:hover {
-    background: #369cb8;
-}
-
-.registerPayment {
-    width: 100% !important;
-}
-</style>
+      methods: {
+    
+      },  
+    
+      computed: {
+        ...mapStores(useRegistryStore)
+      },
+    
+      mounted(){
+        this.RegistryStore.connect()
+      }
+    };
+    </script>
+    
+    <style>
+    .form {
+      display: flex;
+      flex-wrap: nowrap;
+    
+      justify-content: center;    
+      flex-direction: column;
+      background: #fff;
+      margin-top: 5%;
+      margin-left: 20%;
+      margin-right: 20%;
+      margin-bottom: 5%;
+      padding: 30px;
+      box-shadow: 0 0 3px rgba(0, 0, 0, 0.3);
+    }
+    
+    h6{
+      color: #44c4e7;
+    }
+    
+    .form h2 {
+      margin: 0 0 20px;
+      line-height: 1;
+      color: #44c4e7;
+      font-size: 18px;
+      font-weight: 400;
+    }
+    .form input {
+      outline: none;
+      display: block;
+      width: 100%;
+      margin: 0 0 20px;
+      padding: 10px 15px;
+      border: 1px solid #ccc;
+      color: #ccc;
+      font-family: "Roboto";
+      -webkit-box-sizing: border-box;
+      -moz-box-sizing: border-box;
+      box-sizing: border-box;
+      font-size: 14px;
+      font-weight: 400;
+      -webkit-font-smoothing: antialiased;
+      -moz-osx-font-smoothing: grayscale;
+      -webkit-transition: 0.2s linear;
+      -moz-transition: 0.2s linear;
+      -ms-transition: 0.2s linear;
+      -o-transition: 0.2s linear;
+      transition: 0.2s linear;
+    }
+    .form input:focus {
+      color: #333;
+      border: 1px solid #44c4e7;
+    }
+    .form button {
+      cursor: pointer;
+      background: #44c4e7;
+      width: 100%;
+      padding: 10px 15px;
+      border: 0;
+      color: #fff;
+      font-family: "Roboto";
+      font-size: 14px;
+      font-weight: 400;
+    }
+    .form button:hover {
+      background: #369cb8;
+    }
+    
+    .acceptDebt{
+      width: 50% !important;
+    }
+    
+    .rejectDebt{
+      background: rgb(255, 76, 48) !important;
+      width: 50% !important;
+    }
+    
+    .form .rejectDebt:hover {
+      background: rgb(175, 65, 84) !important;
+    }
+    </style>
     
